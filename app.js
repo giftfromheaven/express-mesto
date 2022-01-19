d48b618436cab5d762103155e46201849cb234ff
Ilya Dykin <dddiii06@yandex.ru>
2022-01-19 15:21:34 +0300

    adding cors again

--- app.js
+++ app.js
@@ -20,6 +20,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
+const allowedCors = [
+  "http://praktikumdomainfrontend.nomoredomains.rocks",
+  "https://praktikumdomainfrontend.nomoredomains.rocks",
+  "localhost:3000",
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", true);
+  }
+  const { method } = req;
+  if (method === "OPTIONS") {
+    const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+    const requestHeaders = req.headers["access-control-request-headers"];
+    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+    res.header("Access-Control-Allow-Headers", requestHeaders);
+    return res.end();
+  }
+  return next();
+});
+
 app.post(
   "/signin",
   celebrate({
